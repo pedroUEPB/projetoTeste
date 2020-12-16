31c9288e2fbcd306a0339188a3fdebc10d288205
Paulo <32178749+jpauloregis@users.noreply.github.com>
2020-12-16 20:54:51 -0300

    Update LoginController.js

--- server/src/Controllers/LoginController.js
+++ server/src/Controllers/LoginController.js
@@ -2,27 +2,27 @@ const Aluno = require('../models/ModelAluno')
 const Professor = require('../models/ModelProfessor')
 
 module.exports = {
-    async indexLoginAluno(req, res){
+    async indexLoginAluno(req, res) {
         const user = req.body.user;
         //console.log(user)
         const dados = await Aluno.findOne({
-            where: {usuario: user}
+            where: { usuario: user }
         })
         //console.log(dados);
-        if(!dados){
+        if (!dados) {
             return res.status(200).json("ok")
         }
         return res.status(200).json("opa");
     },
 
-    async indexLoginProfessor(req, res){
+    async indexLoginProfessor(req, res) {
         const user = req.body.user;
         //console.log(user)
         const dados = await Professor.findOne({
-            where: {usuario: user}
+            where: { usuario: user }
         })
         //console.log(dados);
-        if(!dados){
+        if (!dados) {
             return res.status(200).json("ok")
         }
         return res.status(200).json("opa");
