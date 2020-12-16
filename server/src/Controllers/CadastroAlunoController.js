dd7f9dee3474abc1d34e6032a797c846b88df452
Paulo <32178749+jpauloregis@users.noreply.github.com>
2020-12-16 20:54:22 -0300

    Update CadastroAlunoController.js

--- server/src/Controllers/CadastroAlunoController.js
+++ server/src/Controllers/CadastroAlunoController.js
@@ -3,32 +3,32 @@ const Aluno = require('../models/ModelAluno');
 const CadAlunos = require('../models/ModelCadAluno');
 const Projeto = require('../models/ModelProjeto');
 module.exports = {
-    async store(req,res){
-        const {idProj, matric, form} = req.body;
+    async store(req, res) {
+        const { idProj, matric, form } = req.body;
         const al = await Aluno.findOne({
-            where: {matricula: matric}
+            where: { matricula: matric }
         })
-        if(!al){
+        if (!al) {
             return res.status(400).json({
-                Error:['Aluno não encontrado']
+                Error: ['Aluno não encontrado']
             })
         }
-        const cadAluno = await CadAluno.create({fk_aluno: al.id, fk_projeto: idProj, formacao: form})
-        if(!cadAluno){
+        const cadAluno = await CadAluno.create({ fk_aluno: al.id, fk_projeto: idProj, formacao: form })
+        if (!cadAluno) {
             return res.status(400).json({
-                Error:['Não foi possível criar um Cadastro de Aluno']
+                Error: ['Não foi possível criar um Cadastro de Aluno']
             })
         }
         return res.status(200).json(cadAluno);
     },
 
     //PEGAR OS PROJETOS DO ALUNO
-    async projetosAluno(req, res){
-        const {id} = req.body;
+    async projetosAluno(req, res) {
+        const { id } = req.body;
         const projetos = await Projeto.findAll({
             where: { fk_aluno: id }
         })
-        if(!projetos){
+        if (!projetos) {
             return res.status(400).json({
                 Erros: ['Nenhum projeto encontrado']
             })
@@ -37,39 +37,39 @@ module.exports = {
     },
 
     //DELETAR UM CADASTRO
-    async delete(req, res){
+    async delete(req, res) {
         //const {idProj, matric} =  req.body;
         const cadastro = await Aluno.findByPk(req.body.id);
-        
-        if(!cadastro){
+
+        if (!cadastro) {
             return res.status(400).json({
-                Error:['Cadastro não encontrado']
+                Error: ['Cadastro não encontrado']
             })
         }
         await cadastro.destroy();
-        if(!cadastro){
+        if (!cadastro) {
             return res.status(400).json({
-                Error:['Não foi possivel apagar']
+                Error: ['Não foi possivel apagar']
             })
         }
         return res.status(200).json({
-            Sucess:['Cadastro apagado']
+            Sucess: ['Cadastro apagado']
         })
     },
 
     //MUDAR UM CADASTRO
-    async change(req, res){
-        const {id, fk_aluno, fk_projeto, form} = req.body;
+    async change(req, res) {
+        const { id, fk_aluno, fk_projeto, form } = req.body;
         //console.log(cadastro);
         const cad = await CadAlunos.findByPk(id);
-        if(!cad){
+        if (!cad) {
             return res.status(400).json({
-                Error:['Cadastro não encontrado']
+                Error: ['Cadastro não encontrado']
             })
         }
-        await cad.save({id, fk_aluno, fk_projeto, form});
+        await cad.save({ id, fk_aluno, fk_projeto, form });
         return res.status(200).json({
-            Sucess:['Cadastro alterado']
+            Sucess: ['Cadastro alterado']
         })
     }
 }
